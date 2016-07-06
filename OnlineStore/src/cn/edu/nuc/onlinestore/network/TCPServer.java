c445b92b135edb383bcdbfe971b0aa543c3123eb
JavaEEProjectTeam <wangkaikai@126.com>
2016-07-06 09:15:52 +0800

    服务器端完成对购物车请求的响应

--- OnlineStore/src/cn/edu/nuc/onlinestore/network/TCPServer.java
+++ OnlineStore/src/cn/edu/nuc/onlinestore/network/TCPServer.java
@@ -72,20 +72,18 @@ public class TCPServer extends Thread{
 				switch (clientMessage.getType()) {
 					case Request.LOGIN_MESSAGE:   //登录操作
 						userLogin(client, clientMessage, onlineCountLabel);
-						
 						break;
 					case Request.REGISTER_MESSAGE://注册操作
 						userRegister(client, clientMessage);
 						break;
 					case Request.OFFLINE_MESSAGE: //离线操作
-						onlineClient.get(clientMessage).close();          //关闭客户端连接
-						onlineClient.remove(clientMessage.getUsername()); //从在线列表中移除
+						userOffline(clientMessage);
 						break;
 					case Request.SEARCH_MESSAGE:  //检索操作
 						searchGoodsByName(client, clientMessage);
 						break;
 					case Request.PAY_MESSAGE:     //结账操作
-						;
+						userPay(client, clientMessage);
 						break;
 				}
 			} catch (Exception e) {
@@ -98,6 +96,29 @@ public class TCPServer extends Thread{
 				}
 			}
 		}
+	}
+	
+	/**
+	 * 用户结算购物车，执行支付操作
+	 * @throws Exception 向客户端发送消息失败
+	 */
+	private void userPay(Socket client, Request clientMessage) throws Exception {
+		String message = "购买成功，您本次消费" 
+					+ clientMessage.getCart().getTotalPrice() + "元！祝您购物愉快！";
+		Response response = new Response();
+		response.setMessageType(Response.PAY_MESSAGE);
+		response.setMessage(message);
+		IOUtility.persistObject(response, client.getOutputStream());
+	}
+
+	/**
+	 * 用户下线
+	 * @param clientMessage 向客户端发送的消息
+	 * @throws Exception 向客户端发送消息的过程中出现异常
+	 */
+	private void userOffline(Request clientMessage) throws Exception {
+		onlineClient.get(clientMessage).close();          //关闭客户端连接
+		onlineClient.remove(clientMessage.getUsername()); //从在线列表中移除
 	}
 
 	/**
@@ -130,10 +151,6 @@ public class TCPServer extends Thread{
 		user.setUsername(clientMessage.getUsername());
 		user.setPassword(clientMessage.getPassword());
 		IOUtility.writeUserToFile(user);
-		Response response = new Response();
-		response.setResult(true);
-		response.setMessageType(Response.REGISTER_MESSAGE);
-		IOUtility.persistObject(response, client.getOutputStream());
 	}
 
 	/**
@@ -152,17 +169,12 @@ public class TCPServer extends Thread{
 			label.setText("当前在线用户数: " + onlineClient.size());
 			label.updateUI();  //更新界面显示
 		}
+		//设置对客户端的响应信息
 		response.setResult(flag);
 		response.setMessageType(Response.LOGIN_MESSAGE);
+		response.setMessage(clientMessage.getUsername());
+		response.setObj(IOUtility.getAllGoods());  //向客户发送所有商品信息
 		IOUtility.persistObject(response, client.getOutputStream());
-	}
-	
-	/**
-	 * 获得当前在线的用户数
-	 * @return 当前在线的用户数
-	 */
-	public int getOnlineUserCount() {
-		return onlineClient.size();
 	}
 	
 	/**
