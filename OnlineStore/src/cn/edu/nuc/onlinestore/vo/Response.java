ce41d760537caedb5711e207048fa7a5b50e6706
JavaEEProjectTeam <wangkaikai@126.com>
2016-07-06 08:40:41 +0800

    修改了服务器发送给客户端的响应类型，增加了message字段，并删除了所有的无用测试类

--- OnlineStore/src/cn/edu/nuc/onlinestore/vo/Response.java
+++ OnlineStore/src/cn/edu/nuc/onlinestore/vo/Response.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 /**
  * 服务器反馈给客户端的结果信息
+ * 
  * @author 王凯
  *
  * 
@@ -17,37 +18,42 @@ public class Response implements Serializable {
 	/**
 	 * 登录消息
 	 */
-	public static final int LOGIN_MESSAGE = 0; 
-	
+	public static final int LOGIN_MESSAGE = 0;
+
 	/**
 	 * 注册消息
 	 */
-	public static final int REGISTER_MESSAGE = 1; 
-	
+	public static final int REGISTER_MESSAGE = 1;
+
 	/**
 	 * 检索商品消息
 	 */
-	public static final int SEARCH_MESSAGE = 2; 
-	
+	public static final int SEARCH_MESSAGE = 2;
+
 	/**
 	 * 下线消息
 	 */
-	public static final int OFFLINE_MESSAGE = 3; 
-	
+	public static final int OFFLINE_MESSAGE = 3;
+
 	/**
 	 * 结账消息
 	 */
-	public static final int PAY_MESSAGE = 4; 
-	
+	public static final int PAY_MESSAGE = 4;
+
 	/**
 	 * 消息类型
 	 */
 	private int messageType;
-	
+
 	/**
 	 * 反馈结果（成功，失败）
 	 */
 	private boolean result;
+
+	/**
+	 * 服务器端给客户端发的消息
+	 */
+	private String message;
 
 	/**
 	 * 传输的对象
@@ -78,9 +84,19 @@ public class Response implements Serializable {
 		this.obj = obj;
 	}
 
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	@Override
 	public String toString() {
 		return "Response [messageType=" + messageType + ", result=" + result
-				+ ", obj=" + obj + "]";
+				+ ", message=" + message + ", obj=" + obj + "]";
 	}
+
+	
 }
